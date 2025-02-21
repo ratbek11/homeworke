9662e6de0848d0a1834714e893b5220d2f9aa807
ratbek11 <raatbekkarimzhanov@gmail.com>
2025-02-21 18:01:58 +0600

    HW_DID_6

---
+++ lessons/lesson_6.py
@@ -0,0 +1,33 @@
+# class Flyable:
+#     def fly(self):
+#         return 'fling'
+#
+# class Swimmable:
+#     def swim(self):
+#         return 'swiming'
+#
+# class Duck(Flyable,Swimmable):
+#     def make_sound(self):
+#         return 'kra kra '
+#
+# donald_duck = Duck()
+# print(donald_duck.fly())
+# print(donald_duck.swim())
+
+
+#rombovidnaya nasledovanie
+
+# class Animal:
+#     def make_sound(self):
+#         return "izdayot zvuk"
+#
+# class Swimmable(Animal):
+#     def swim(self):
+#         return 'swiming'
+#
+# class Flyable(Animal):
+#     def fly(self):/
+        # return 'fling'
+# //
+
+import sqlite3
